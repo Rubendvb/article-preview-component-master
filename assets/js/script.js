9501b9bf158821ec7b248506a43ca8eecae7bee5
Rubendvb <rubenvasquez556@gmail.com>
2022-06-25 21:33:55 -0300

    Finalizando projeto

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
-let btn = document.querySelector(".containerInformaçoes__btn");
+let btn = document.querySelector(".containerInformacoes__btn");
 let icones = document.querySelector(".iconesContainer");
+let teste = document.getElementById("teste");
 
 btn.addEventListener("click", () => {
   icones.classList.toggle("active");
